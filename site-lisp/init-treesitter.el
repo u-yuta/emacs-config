a55c68c1675bdc61612903d43f133bcc733d02e1
u-yuta <u-yuta@users.noreply.github.com>
2025-02-08 17:56:41 +0900

    chore: change license from MIT to GPL-3.0

--- site-lisp/init-treesitter.el
+++ site-lisp/init-treesitter.el
@@ -1,3 +1,21 @@
+;;; init-treesitter.el --- Tree-sitter configurations -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 u-yuta
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 ;; M-x treesit-install-language-grammar でインストールする language grammer の設定
 ;; https://gist.github.com/slewsys/4ee95a67577e4df64d5f716c30420555
 ;;
