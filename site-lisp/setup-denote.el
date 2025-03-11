5dd304ca140e42bb4c28707fe0864a9e67a581c1
u-yuta <u-yuta@users.noreply.github.com>
2025-03-12 00:25:24 +0900

    add site-lisp/setup-denote.el

---
+++ site-lisp/setup-denote.el
@@ -0,0 +1,36 @@
+;;; setup-denote.el --- Denote configurations -*- lexical-binding: t -*
+
+;; Copyright (C) 2025 u-yuta
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;; Denoteをテスト使用中
+(use-package denote
+  :ensure t
+  :hook (dired-mode . denote-dired-mode)
+  :after org
+  :bind
+  (("C-c n n" . denote)
+   ("C-c n r" . denote-rename-file)
+   ("C-c n l" . denote-link)
+   ("C-c n b" . denote-backlinks)
+   ("C-c n d" . denote-sort-dired))
+  :config
+  (setopt denote-directory (file-name-concat org-directory "lab-notes/2025/03"))  ;; test
+
+  ;; Automatically rename Denote buffers when opening them so that
+  ;; instead of their long file name they have, for example, a literal
+  ;; "[D]" followed by the file's title.  Read the doc string of
+  ;; `denote-rename-buffer-format' for how to modify this.
+  (denote-rename-buffer-mode 1))
