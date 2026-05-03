7c4ec588ff5a9c2988a4569111e169bac97dcf53
u-yuta <u-yuta@users.noreply.github.com>
2026-05-03 11:40:01 +0900

    fix: remove edraw-org related code

--- site-lisp/setup-org-mode.el
+++ site-lisp/setup-org-mode.el
@@ -484,14 +484,6 @@
       )))
   )
 
-;; When using the org-export-in-background option (when using the
-;; asynchronous export function), the following settings are
-;; required. This is because Emacs started in a separate process does
-;; not load org.el but only ox.el.
-(with-eval-after-load "ox"
-  (require 'edraw-org)
-  (edraw-org-setup-exporter))
-
 ;; ob-mermaid
 (use-package ob-mermaid
   :ensure t
